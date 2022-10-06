d84041ed33476fb3cba0d751ddebddec4de613c4
fanart13 <110786757+leidert11@users.noreply.github.com>
2022-10-06 06:39:01 -0500

    Update piedra-papel-tijera_function.js

--- piedra-papel-tijera_function.js
+++ piedra-papel-tijera_function.js
@@ -3,11 +3,57 @@ let veces = function(){
     }
     const cantidad = veces()
 
-    let pc = function(){
-        let maquina = (Math.floor(Math.random() * 3 + 1))
-        return (maquina)
-    }
+    
     const ia = pc;
+    for (i=0; i<veces; i++){
+        
+        let jugador= function(){
+        let jugador1=+prompt("Escoja la opcion que desea piedra-papel-tijera")
+        switch(opcion){
+            case 1: papel
+            break;
+            case 2: tijeras
+            break;
+            case 3: Piedra
+            break;
+        }
+        let pc = function(){
+            let maquina = (Math.floor(Math.random() * 3 + 1))
+            return (maquina)
+        }
+        if(pc == opcion){
+            alert("hay un empate")
+        }
+        else if(pc == tijeras && jugador == papel){
+            alert("Ha ganado la maquina")
+        }
+        else if(pc == Piedra && jugador == tijeras){
+            alert("Ha ganado la maquina")
+        }
+        else if(pc == papel && jugador == Piedra){
+          alert("Ha ganado la maquina")    
+        }
+        else if(pc == tijeras && jugador == papel){
+            alert("Ha ganado la maquina")    
+          }
+        else if(jugador == tijeras && pc == papel){
+          alert("Ha ganado el jugador") 
+        }
+        else if(jugador == Piedra && pc == tijeras){
+            alert("Ha ganado el jugador")
+        }
+        else if(jugador == papel && pc == Piedra){
+            alert("Ha ganado el jugador")
+        }
+        else if(jugador == tijeras && pc == papel){
+            alert("Ha ganado el jugador")
+        }
+
+
+       
+        return (jugador1)
+    }
+}
 
     let contajugador = function (){
         playerone++
@@ -17,51 +63,6 @@ let veces = function(){
     let contamaquina = function (){
         ia++
         return (ia)
-    }
-    
-    for (i=0; i<veces; i++){
-            let jugador= function(){
-            let jugador1=+prompt("Escoja la opcion que desea piedra-papel-tijera")
-            switch(opcion){
-                case 1: papel
-                break;
-                case 2: tijeras
-                break;
-                case 3: Piedra
-                break;
-            }
-            if(pc == opcion){
-                alert("hay un empate")
-            }
-            else if(pc == tijeras && jugador == papel){
-                alert("Ha ganado la maquina")
-            }
-            else if(pc == Piedra && jugador == tijeras){
-                alert("Ha ganado la maquina")
-            }
-            else if(pc == papel && jugador == Piedra){
-              alert("Ha ganado la maquina")    
-            }
-            else if(pc == tijeras && jugador == papel){
-                alert("Ha ganado la maquina")    
-              }
-            else if(jugador == tijeras && pc == papel){
-              alert("Ha ganado el jugador") 
-            }
-            else if(jugador == Piedra && pc == tijeras){
-                alert("Ha ganado el jugador")
-            }
-            else if(jugador == papel && pc == Piedra){
-                alert("Ha ganado el jugador")
-            }
-            else if(jugador == tijeras && pc == papel){
-                alert("Ha ganado el jugador")
-            }
-    
-    
-           
-            return (jugador1)
-        }
     }
        
         
